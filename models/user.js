92cf1ebf1fe5df50c63ce131c9d0004dd52d96f0
Michael Duran <mad2116@gmail.com>
2016-04-05 12:42:04 -0700

    possible refactoring of models to let users have their own tags for each article

--- models/user.js
+++ models/user.js
@@ -2,12 +2,54 @@ var mongoose = require("mongoose"),
     debug    = require("debug")("app:models");
 
 
+var userArticleSchema = new mongoose.Schema({
+  article:     {type: mongoose.Schema.Types.ObjectId, ref: "Article"},
+  title:        String,
+  description:  String,
+  thumbnailImg: String,
+  tags:         [String]
+});
+
+userArticleSchema.methods.addTag = function(newTag) {
+  newTag = newTag.toLowerCase();
+  if (this.tags.indexOf(newTag) === -1) {
+    this.tags.push(newTag);
+    return newTag;
+  } else {
+    return null;
+  }
+};
+
+// var user    = User.findById(...);
+// var article = Article.findById(...);
+// var usersArticle = user.articles.push({
+//   article:      article._id,
+//   title:        article.title,
+//   description:  article.description,
+//   thumbnailImg: article.thumbnailImg,
+//   tags:         ['puglife', 'yolo']
+// });
+// possibly:
+// var usersArticle = user.articles[user.articles.length-1];
+// user.save();
+
+// How you add tags by default.
+// usersArticle.tags.push('lavidaloca');
+// user.save();
+
+// How you do it now.
+// usersArticle.addTag('newTag');
+// user.save();
+// if (!usersArticle.addTag('newTag')) {
+//   res.send('Tag already added!');
+// }
+
 var userSchema = new mongoose.Schema({
   firstName: {type: String, required: true},
-  lastName: {type: String, required: true},
-  username: {type: String, required: true, unique: true},
-  email: {type: String, required: true, unique: true},
-  articles: [{type: mongoose.Schema.Types.ObjectId, ref: "Article"}]
+  lastName:  {type: String, required: true},
+  username:  {type: String, required: true, unique: true},
+  email:     {type: String, required: true, unique: true},
+  articles:  [userArticleSchema]
 });
 
 // Add bcrypt hashing to model (works on a password field)!

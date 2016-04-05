92ee7a199077011d16594a7b9cf717307feb3711
Michael Duran <mad2116@gmail.com>
2016-04-04 18:02:01 -0700

    add authentication vars

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,10 @@ var _ = require('lodash');
 
 var localEnvVars = {
   TITLE:      'communico_app',
-  SAFE_TITLE: 'communico_app'
+  SAFE_TITLE: 'communico_app',
+  COOKIE_SECRET:  'notsosecretnowareyou',
+  SESSION_SECRET: 'anotherfoolishsecret',
+  TOKEN_SECRET:   'andafinalsecretsadasitis'
 };
 
 // Merge all environmental variables into one object.

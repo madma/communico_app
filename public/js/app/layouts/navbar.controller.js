12ca7cfaddea2670f45ba0cdeb59a6066fe63762
Michael Duran <mad2116@gmail.com>
2016-05-02 19:56:57 -0700

    add links to "about" and "try it!" anchors

--- public/js/app/layouts/navbar.controller.js
+++ public/js/app/layouts/navbar.controller.js
@@ -5,13 +5,20 @@
     .module("app")
     .controller("NavbarController", NavbarController);
 
-  NavbarController.$inject = ["$log", "authService", "$state"];
+  NavbarController.$inject = ["$log", "authService", "$state", "$location", "$anchorScroll"];
 
-  function NavbarController($log, authService, $state) {
+  function NavbarController($log, authService, $state, $location, $anchorScroll) {
     var vm = this;
 
     vm.authService = authService;
     vm.logOut      = logOut;
+    vm.goToDiv     = goToDiv;
+
+
+    function goToDiv(id) {
+      $location.hash(id);
+      $anchorScroll();
+    }
 
     // Why put the $state change here instead of in the
     // auth service? Because I only want the auth service to
